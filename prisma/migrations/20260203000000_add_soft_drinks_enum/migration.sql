fe04296484b4b61bca356d2411f4179fd28bd2bd
Anil <anilparajuli580@gmail.com>
2026-02-03 13:04:02 +0545

    chnges issue

---
+++ prisma/migrations/20260203000000_add_soft_drinks_enum/migration.sql
@@ -0,0 +1,5 @@
+-- AlterEnum
+ALTER TYPE "Category" ADD VALUE 'SOFT_DRINKS';
+
+-- Also rename DRINKS to SOFT_DRINKS if needed (optional)
+-- First add the new value, then you can migrate existing data
